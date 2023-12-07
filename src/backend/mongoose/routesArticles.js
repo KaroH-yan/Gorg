7991a7b3767e50d0de4618f19cef09117c218eca
Gev <gev@Mac-mini-TestX2.local>
2023-12-07 20:59:30 +0400

    fix uploader

--- src/backend/mongoose/routesArticles.js
+++ src/backend/mongoose/routesArticles.js
@@ -1,39 +1,73 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const express = require("express");
 const Model = require("./modelsArticles");
-const xlsx = require('xlsx'); 
+const xlsx = require("xlsx");
 const multer = require("multer");
+const XLSX = require("xlsx");
 const router = express.Router();
 
 global._basedir = __dirname;
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, path.join(_basedir, "uploads"));
-    },
-    filename: (req, file, cb) => {
-      cb(null, file.originalname);
-    },
-  });
-  
-  const upload = multer({ storage: storage });
+const upload = multer({
+  limits: {
+    fileSize: 1000000,
+  },
+});
 
-router.post('/addListOfArticles', upload.single("uploadfile"), (req, res) => {
-    try {
-      const filePath = req.file;
-      importExcelData2MongoDB(filePath);
-      res.json({
-        msg: "File Uploaded",
-        file: req.file?.filename,
+router.post("/addListOfArticles", upload.single("file"), async (req, res) => {
+  try {
+    const workbook = XLSX.read(req.file.buffer, { type: "buffer" });
+
+    const sheetName = workbook.SheetNames[0];
+    const sheet = workbook.Sheets[sheetName];
+
+    const dataArray = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+
+    const keys = dataArray[0];
+
+    const resultArray = dataArray.slice(1).map(function (row) {
+      const obj = {};
+      keys.forEach(function (key, index) {
+        obj[key] = row[index];
       });
-    } catch (err) {
-      console.log("Error uploading file:", err);
-      res.status(500).json({ error: "Failed to upload file" });
-    }
-  });
+      return obj;
+    });
+    await importExcelData2MongoDB(resultArray);
 
-  async function importExcelData2MongoDB(filePath) {
-    console.log(filePath, "filePath")
+    res.status(300).json({
+      msg: "File Uploaded",
+      fileName: req.file?.filename,
+      data: resultArray,
+    });
+  } catch (err) {
+    console.log("Error uploading file:", err);
+    res.status(500).json({ error: "Failed to upload file" });
   }
+});
+
+async function importExcelData2MongoDB(fileContent) {
+  try {
+    const workbook = XLSX.read(fileContent, { type: "buffer" });
+
+    const sheetName = workbook.SheetNames[0];
+    const sheet = workbook.Sheets[sheetName];
+
+    const dataArray = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+
+    const keys = dataArray[0];
+
+    const resultArray = dataArray.slice(1).map(function (row) {
+      const obj = {};
+      keys.forEach(function (key, index) {
+        obj[key] = row[index];
+      });
+      return obj;
+    });
+
+    return resultArray;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 module.exports = router;
--- src/pages/AricleList.js
+++ src/pages/AricleList.js
@@ -1,110 +1,49 @@
-import React, {useState} from 'react'
-import { Button, message, Upload } from 'antd'
+import React, { useState } from "react";
+import { Button, message, Upload } from "antd";
 import * as XLSX from "xlsx";
-import {UploadOutlined} from '@ant-design/icons'
+import { UploadOutlined } from "@ant-design/icons";
 
+export default () => {
+  const [fileList, setFileList] = useState([]);
+  const [uploading, setUploading] = useState(false);
+  const [articleList, setArticleList] = useState([]);
 
-export default ()=>{
-    const [fileList, setFileList] = useState([]);
-    const [uploading, setUploading] = useState(false);
+  const url = process.env.REACT_APP_ADD_PARTNER_ARTICLES_URI;
 
-    const url=process.env.REACT_APP_ADD_PARTNER_ARTICLES_URI
+  async function addArticleList(data) {
+    const res = await fetch(url, {
+      method: "POST",
+      body: data,
+    }).then((response) => response.json());
+    setArticleList([...articleList, ...res.data]);
+    setUploading(false);
+  }
 
-    async function addArticleList(url, data) {
-        const fileReader = new FileReader();
-        
-        const workbook = XLSX.readFile(data.name, );
-        console.log(workbook, 'workbook')
-        console.log(workbook.Sheets, 'Sheets')
-        for (const Sheet in workbook.Sheets) {
-            // var XL_row_object =
-            let ws = workbook.Sheets["Sheet1"]
-            
-            console.log(ws, "ws")
-          }
-        
-            console.log(data, "data")
-    }
-          /*  try {
-              const { result } = event.target;
-              const workbook = XLSX.read(result, { type: "binary" });
-              let data = [];
-              for (const Sheet in workbook.Sheets) {
-                // var XL_row_object =
-                XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-                if (workbook.Sheets.hasOwnProperty(Sheet)) {
-                  data = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[Sheet]);
-                  data.forEach(x => {
-                    addItem(x.sku, x.description, parseInt(x.quantity), parseFloat(x.cost));
-                  });
-                }
-              }
-              message.success("upload success!");
-              console.log(data, "data");
-            } catch (e) {
-              message = message.error("file type is incorrect!");
-            }
-          };
-          fileReader.readAsBinaryString(files[0]);
-        const response = await fetch(url,
-            {
-                 method: "post",
-                 headers: {"Content-Type": "application/JSON"},
-                 body: JSON.stringify({name: data.name, PartnerId:'asdas'})
-            });
-            
-      }*/
-    const handleUpload = () => {
-      const formData = new FormData();
-     
-      fileList.forEach((file) => {
-        formData.append('files[]', file);
-      });
-      setUploading(true);
-      console.log("fileList", fileList)
-      fileList.forEach((file) => { addArticleList(url, file)})
-      
-     
-      
+  const handleUpload = () => {
+    setUploading(true);
+    const formData = new FormData();
+    formData.append("file", fileList[0], fileList[0].name);
 
-      // You can use any AJAX library you like
-      //fetch('https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188', {
-    /*    fetch(process.env.REACT_APP_ADD_PARTNER_ARTICLES_URI, {
-        method: 'POST',
-        body: {fromdata:formData, ParterId:"asdas"}
-      })
-        .then((res) => res.json())
-        .then(() => {
-          setFileList([]);
-          message.success('upload successfully.');
-        })
-        .catch(() => {
-          message.error('upload failed.');
-        })
-        .finally(() => {
-          setUploading(false);
-        });*/
-    };
-    const props = {
-      onRemove: (file) => {
-        const index = fileList.indexOf(file);
-        const newFileList = fileList.slice();
-        newFileList.splice(index, 1);
-        setFileList(newFileList);
-      },
-      beforeUpload: (file) => {
-        setFileList([...fileList, file]);
-        return false;
-      },
-      fileList,
-    };
-    return(
-        <>
-         <Upload>
-        <Button icon={<UploadOutlined />}>Click to Upload</Button>
-    </Upload>
+    addArticleList(formData).catch(() => setUploading(false));
+  };
 
-        <Upload {...props}>
+  const props = {
+    accept: ".xlsx",
+    onRemove: (file) => {
+      const index = fileList.indexOf(file);
+      const newFileList = fileList.slice();
+      newFileList.splice(index, 1);
+      setFileList(newFileList);
+    },
+    beforeUpload: (file) => {
+      setFileList([...fileList, file]);
+      return false;
+    },
+    fileList,
+  };
+  return (
+    <>
+      <Upload {...props}>
         <Button icon={<UploadOutlined />}>Select File</Button>
       </Upload>
       <Button
@@ -116,9 +55,10 @@ export default ()=>{
           marginTop: 16,
         }}
       >
-        {uploading ? 'Uploading' : 'Start Upload'}
+        {uploading ? "Uploading" : "Start Upload"}
       </Button>
-      </>
-   
-    )
-}
+
+      {JSON.stringify(articleList, null, 2)}
+    </>
+  );
+};
